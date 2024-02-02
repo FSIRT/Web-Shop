caa0acc6c89b56bd5fa537e4c41bd19b9c6940a7
Lukas Buck <Lukas.buck@e-mail.de>
2024-02-02 20:11:43 +0100

    Entfernen der Platzhalterdatei

--- services/userProvider/decodePassword.php
+++
@@ -1,7 +0,0 @@
-<?php
-function hashPassword($password)
-{
-    $hashedPassword = hash('sha512', $password);
-    return $hashedPassword;
-}
-?>
--- services/userProvider/saveScreenResolution.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-if (isset($_POST['resolution'])) {
-    $_SESSION['resolution'] = $_POST['resolution'];
-}
-?>
